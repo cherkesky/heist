171aaa94a9639e9979fb42e951bd73e28bf64d41
Cherkesky <guy@cherkesky.com>
2020-04-30 14:41:44 -0500

    TeamMember class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace heist{
+
+public class TeamMember {
+
+public string name {get; set;}
+public  int skillLevel {get; set;}
+public double courageFactor {get; set;}
+
+
+}
+}
